b2e6ca360c979eca9fe680c619f37262ac5f02f4
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-20 17:31:02 +0900

    [add]userテーブルの追加

--- opt/accounts/models.py
+++ opt/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(max_length=240)
+  password = models.TextField()
+  salt = models.TextField()
+  urltoken = models.TextField()
+  status = models.IntegerField(default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
