fb1d218a7ed3d3cb2644a2ba9082a8d05cddca58
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-21 10:45:06 +0900

    [update]テーブルの変更

--- opt/accounts/models.py
+++ opt/accounts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
   name = models.CharField(max_length=100)
   email = models.EmailField(max_length=240)
-  password = models.TextField()
-  salt = models.TextField()
-  urltoken = models.TextField()
+  password = models.CharField(max_length=256)
+  salt = models.CharField(max_length=256)
+  urltoken = models.UUIDField()
   status = models.IntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
