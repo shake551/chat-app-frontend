70284c5be3cdcec091e5f93099bdaf59f74a63b4
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-22 23:12:36 +0900

    [add]メール認証(実際にメールは送信せずconsoleでのみ表示

--- opt/accounts/views.py
+++ opt/accounts/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
 
 import uuid
 import os
@@ -16,7 +17,7 @@ def hash_password(my_password, salt):
     safe_pass = hashlib.sha256(salt + password).hexdigest()
     return safe_pass
 
-    
+
 @api_view(['POST'])
 @csrf_exempt
 def pre_signup(request):
@@ -34,6 +35,14 @@ def pre_signup(request):
     serializer = UserSerializer(data=insert_data)
 
     if serializer.is_valid():
-        serializer.save()
+        new_data = serializer.save()
+        verify_url = 'http://0.0.0.0:8000/api/verifyaccount?token=' + str(new_data.urltoken)
+        send_mail(
+            '本登録のお願い',
+            '以下のリンクにアクセスして本登録を完了してください\n' + verify_url,
+            'admin@example.com',
+            [new_data.email],
+            fail_silently=False,
+        )
         return JsonResponse(serializer.data, status=201)
     return JsonResponse(serializer.errors, status=400)
