1b1cdaf1dcf6d0ae09ca627607bee0b1d79667d5
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-24 00:42:10 +0900

    [update]dbのカラム変更

--- opt/accounts/models.py
+++ opt/accounts/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
   name = models.CharField(max_length=100)
   email = models.EmailField(max_length=240)
-  password = models.CharField(max_length=256)
-  salt = models.CharField(max_length=256)
+  password = models.CharField(max_length=255)
+  salt = models.BinaryField()
   urltoken = models.UUIDField()
   status = models.IntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
