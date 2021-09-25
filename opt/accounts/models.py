1d338e518a7a21b4752aeed880224ce3a86c5af2
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-25 16:00:38 +0900

    [update]テーブル変更 saltの変更

--- opt/accounts/models.py
+++ opt/accounts/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
   name = models.CharField(max_length=100)
   email = models.EmailField(max_length=240)
   password = models.CharField(max_length=255)
-  salt = models.BinaryField()
+  salt = models.CharField(max_length=255)
   urltoken = models.UUIDField()
   status = models.IntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
