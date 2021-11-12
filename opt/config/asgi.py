4e95b45b5b9141c6bd62d9b6004b8b26879967a8
shake <hiroki.yamako@ezweb.ne.jp>
2021-11-12 10:42:20 +0900

    [add]websocketでの通信

--- opt/config/asgi.py
+++ opt/config/asgi.py
@@ -13,4 +13,20 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+#application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter
+
+from channels.routing import URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
