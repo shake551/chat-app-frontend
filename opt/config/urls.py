d1d45329eb3419cefc6199a61e4fb1b5e85389a6
shake <hiroki.yamako@ezweb.ne.jp>
2021-11-16 00:26:52 +0900

    [update]chatAPIのurl変更

--- opt/config/urls.py
+++ opt/config/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('chat/', include('chat.urls')),
+    path('api/chat/', include('chat.urls')),
 ]
