589ff03f122e104e0858e0ad168c2b060ae71384
shake <hiroki.yamako@ezweb.ne.jp>
2022-03-13 17:38:32 +0900

    [add]ユーザーの仮登録

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from  'react-router-dom'
 
 import Home from './components/Home';
+import SignupForm from './components/signup/SignupForm';
+import SignupComplete from './components/signup/SignupComplete';
 import LoginForm from './components/LoginForm';
 import LoginUser from './components/LoginUser';
 import Message from './components/chat/Message';
@@ -12,6 +14,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<SignupForm />} />
+        <Route exact path="/complete" element={<SignupComplete />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/user" element={<LoginUser />} />
         <Route path="/chat/*" element={<Message />} />
---
+++ app/src/components/signup/SignupComplete.js
@@ -0,0 +1,11 @@
+
+function SignupComplete() {
+  return(
+    <div>
+      <h1>仮登録完了</h1>
+      <p>仮登録が完了しました．ご登録のメールアドレスにメールを送信しました．そちらから本登録を完了させてください．</p>
+    </div>
+  )
+}
+
+export default SignupComplete
