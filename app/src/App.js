710a3ff1234ae97429b7e340dfdf421bbe4f7f55
shake <hiroki.yamako@ezweb.ne.jp>
2022-03-14 22:00:42 +0900

    [add]ユーザー本登録ページ作成

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from  'react-router-dom'
 import Home from './components/Home';
 import SignupForm from './components/signup/SignupForm';
 import SignupComplete from './components/signup/SignupComplete';
+import VerifyUser from './components/signup/VerifyUser';
 import LoginForm from './components/LoginForm';
 import LoginUser from './components/LoginUser';
 import Message from './components/chat/Message';
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/signup" element={<SignupForm />} />
         <Route exact path="/complete" element={<SignupComplete />} />
+        <Route exact path="/verify/*" element={<VerifyUser />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/user" element={<LoginUser />} />
         <Route path="/chat/*" element={<Message />} />
