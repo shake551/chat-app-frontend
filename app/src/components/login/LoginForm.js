7e8c36edd8c586eb68b2f3a40ca340a5b7d9c3bf
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-03 17:53:41 +0900

    [update]クラスコンポーネントを関数コンポーネントに修正 #28

--- app/src/components/login/LoginForm.js
+++ app/src/components/login/LoginForm.js
@@ -1,77 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 import LoginError from "./LoginError";
+import userToken from "../user/UserToken";
 
-class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            password: '',
-            error: 0,
-        };
+const LoginForm = () => {
+    const [loginForm, setForm] = useState({
+        name: '',
+        password: ''
+    });
+    const [error, setError] = useState(0);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const handleChange = (event) => {
+        const area_name = event.target.name;
+        const value = event.target.value;
+
+        setForm({...loginForm, [area_name]: value});
     }
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value});
-    }
-
-    handleSubmit(event) {
-        const data = {
-            name: this.state.name,
-            password: this.state.password,
-        };
-
-        axios.post('http://0.0.0.0:8000/api/accounts/login/', data)
+    const handleSubmit = (event) => {
+        axios.post('http://0.0.0.0:8000/api/accounts/login/', loginForm)
             .then(res => {
-                // ローカルストレージにtokenを保存
-                window.localStorage.setItem('refresh_token', res.data.token.refresh_token);
-                window.localStorage.setItem('access_token', res.data.token.access_token);
+                const success = userToken(res.data.token);
+                if (!success) {
+                    throw new Error();
+                }
 
                 // 画面遷移
                 window.location.href = '/user/home';
             })
             .catch(err => {
-                this.setState({
-                    error: 1,
+                setError(1);
+                setForm({
                     name: '',
-                    password: '',
+                    password: ''
                 });
             })
         event.preventDefault();
     }
 
-    render() {
-        return (
-            <><LoginError
-                error={this.state.error}/>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input
-                            name="name"
-                            type="text"
-                            value={this.state.name}
-                            onChange={this.handleChange}/>
-                    </label>
-                    <br/>
-                    <label>
-                        Password:
-                        <input
-                            name="password"
-                            type="text"
-                            value={this.state.password}
-                            onChange={this.handleChange}/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </>
-        );
-    }
+    return (
+        <><LoginError
+            error={error}/>
+            <form onSubmit={(event ) => handleSubmit(event)}>
+                <label>
+                    Name:
+                    <input
+                        name="name"
+                        type="text"
+                        value={loginForm.name}
+                        onChange={(event) => handleChange(event)}/>
+                </label>
+                <br/>
+                <label>
+                    Password:
+                    <input
+                        name="password"
+                        type="text"
+                        value={loginForm.password}
+                        onChange={(event) => handleChange(event)}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </>
+    );
 }
 
 export default LoginForm;
