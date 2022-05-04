96df73d6cb92edadca0c5452efeee5e631830afb
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-04 12:33:58 +0900

    [update]仮登録完了のスタイリング

--- app/src/components/signup/SignupComplete.js
+++ app/src/components/signup/SignupComplete.js
@@ -1,10 +1,20 @@
+import styled from "styled-components";
+
+const CompleteMessage = styled.p`
+  font-size: 25px;
+  margin: 30px;
+  line-height: 2;
+`;
+
 function SignupComplete() {
     return (
         <div>
-            <h1>仮登録完了</h1>
-            <p>仮登録が完了しました．ご登録のメールアドレスにメールを送信しました．そちらから本登録を完了させてください．</p>
+            <CompleteMessage>
+                仮登録が完了しました．ご登録のメールアドレスにメールを送信しました．
+                添付されているURLにアクセスし、本登録を完了してください．
+            </CompleteMessage>
         </div>
-    )
+    );
 }
 
 export default SignupComplete;
