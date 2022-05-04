78ed05ff00dfaaeb1eb7830c52deb25393403050
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-04 12:21:02 +0900

    [update]エラー表示のスタイリング

--- app/src/components/signup/SignupError.js
+++ app/src/components/signup/SignupError.js
@@ -1,7 +1,20 @@
-function SignupError(props) {
+import styled from "styled-components";
+
+const ErrorDisplay = styled.div`
+  text-align: center;
+  color: #ff0000;
+  background-color: #fccfcf;
+  border: 1px solid #ff0000;
+  border-radius: 20px;
+  margin: 10px;
+`;
+
+const SignupError = (props) => {
     if (props.error) {
         return (
-            <h3>サインアップに失敗しました</h3>
+            <ErrorDisplay>
+                <h3>サインアップに失敗しました</h3>
+            </ErrorDisplay>
         );
     } else {
         return (<span/>)
