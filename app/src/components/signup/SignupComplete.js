271a50b5e3e2e616645a06bf266cfb5e3ca767c2
shake551 <73379887+shake551@users.noreply.github.com>
2022-05-07 12:10:19 +0900

    [update]仮登録完了画面へのベーススタイルの追加

--- app/src/components/signup/SignupComplete.js
+++ app/src/components/signup/SignupComplete.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
+import HomeBase from "../HomeBase";
 
 const CompleteMessage = styled.p`
   font-size: 25px;
-  margin: 30px;
+  margin: 70px 30px;
   line-height: 2;
 `;
 
 const SignupComplete = () => {
     return (
         <div>
+            <HomeBase/>
             <CompleteMessage>
                 仮登録が完了しました．ご登録のメールアドレスにメールを送信しました．
                 添付されているURLにアクセスし、本登録を完了してください．
